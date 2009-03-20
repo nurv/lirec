0fe34509d4836efcb61a562ca80d190dd47e77d4
dave <dave@dcc22606-d844-4505-a083-a3dbd7937ca2>
2009-03-20 14:58:11 +0000

    turned multiple face detect back on
    git-svn-id: http://svn.lirec.eu@30 dcc22606-d844-4505-a083-a3dbd7937ca2

--- foam/trunk/simple-faceident/src/main.cpp
+++ foam/trunk/simple-faceident/src/main.cpp
@@ -250,7 +250,7 @@ void detect_and_draw( IplImage* img )
         double t = (double)cvGetTickCount();
         CvSeq* faces = cvHaarDetectObjects( small_img, cascade, storage,
                                             1.1, 2, 0
-                                            |CV_HAAR_FIND_BIGGEST_OBJECT
+                                            //|CV_HAAR_FIND_BIGGEST_OBJECT
                                             //|CV_HAAR_DO_ROUGH_SEARCH
                                             //|CV_HAAR_DO_CANNY_PRUNING
                                             //|CV_HAAR_SCALE_IMAGE
