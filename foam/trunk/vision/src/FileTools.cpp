8387e47b1852102b009065e7caec63d582afba17
dave <dave@dcc22606-d844-4505-a083-a3dbd7937ca2>
2009-08-03 13:03:55 +0000

    added forgotten files
    git-svn-id: http://svn.lirec.eu@99 dcc22606-d844-4505-a083-a3dbd7937ca2

---
+++ foam/trunk/vision/src/FileTools.cpp
@@ -0,0 +1,35 @@
+#include <glob.h>
+#include "FileTools.h"
+// Copyright (C) 2009 foam
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+
+using namespace std;
+
+vector<string> Glob(const string &path)
+{
+	// todo windoze version
+	glob_t g;
+	vector<string> ret;
+	glob(path.c_str(),GLOB_PERIOD,NULL,&g);
+	for (unsigned int n=0; n<g.gl_pathc; n++)
+	{
+		string path=g.gl_pathv[n];
+		ret.push_back(path);
+	}
+	globfree (&g);
+	return ret;
+}
