607379c0c903b18caa8bfaeb606dc39c7f7cb9ee
KDucasse <KDucasse@dcc22606-d844-4505-a083-a3dbd7937ca2>
2009-09-24 12:00:26 +0000

    git-svn-id: http://svn.lirec.eu@125 dcc22606-d844-4505-a083-a3dbd7937ca2

---
+++ UH/SAMGAR/SAMGAR Gui/SourceFiles/LogComponent.h
@@ -0,0 +1,27 @@
+/*
+#include <juce_amalgamated.h>
+
+
+
+class LogComponent  : public Component, public ButtonListener
+{
+public:
+
+    LogComponent ();
+    ~LogComponent();
+
+    void resized();
+    void buttonClicked (Button* buttonThatWasClicked);
+
+    juce_UseDebuggingNewOperator
+
+private:
+
+    Label* helloWorldLabel;
+    TextButton* quitButton;
+
+    LogComponent (const LogComponent&);
+    const LogComponent& operator= (const LogComponent&);
+};
+
+*/
