2443355c5ae27130c644e1e2e2ed64afe592acb8
michael.kriegel <michael.kriegel@dcc22606-d844-4505-a083-a3dbd7937ca2>
2009-10-13 11:36:03 +0000

    replaced the example mind that comes with this with the latest version of FAtiMA (taken from this SVN 12/10/09 and compiled into a jar, previously this example used a very old version of FAtiMA from FearNot's sourceforge site)
    git-svn-id: http://svn.lirec.eu@145 dcc22606-d844-4505-a083-a3dbd7937ca2

--- ION/trunk/Lirec/fatima-bin/ArchitectureTestAgent.bat
+++ ION/trunk/Lirec/fatima-bin/ArchitectureTestAgent.bat
@@ -1,1 +1,1 @@
-java -cp "FAtiMA.jar" FAtiMA.Agent localhost 46874 true SpiritOfTheBuilding M TestAgent Spirit isTestMind:True
+java -cp "FAtiMA.jar" FAtiMA.Agent ArchitectureTest SpiritOfTheBuilding
