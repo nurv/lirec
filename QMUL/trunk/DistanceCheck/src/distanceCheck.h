bd7d27108b708c43b37dbb49be6139043aabc07e
ginevra <ginevra@dcc22606-d844-4505-a083-a3dbd7937ca2>
2009-10-19 16:05:53 +0000

    git-svn-id: http://svn.lirec.eu@167 dcc22606-d844-4505-a083-a3dbd7937ca2

--- QMUL/trunk/DistanceCheck/src/distanceCheck.h
+++ QMUL/trunk/DistanceCheck/src/distanceCheck.h
@@ -1,9 +1,31 @@
-/* 
-AUTHOR: Ginevra Castellano
-Queen Mary University of London
-DATE: 03/2009
-VERSION: 1.0
-*/
+// AUTHOR: Ginevra Castellano
+// Queen Mary University of London
+// DATE: 03/2009
+// VERSION: 1.0
+
+// Copyright (C) 2009 Ginevra Castellano
+
+// This file is part of the DistanceCheck program.
+
+// DistanceCheck is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// DistanceCheck is based on the face detection code provided by OpenCV.
+// OpenCV: Copyright (C) 2000-2006, Intel Corporation, all rights reserved.
+// Third party copyrights are property of their respective owners.
+// See OpenCV_license.txt, in the program folder, for details.
+
+
 
 #ifndef _DISTANCE_CHECK
 #define _DISTANCE_CHECK
