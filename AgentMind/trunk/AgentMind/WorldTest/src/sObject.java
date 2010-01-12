7cbc110fb7c6393fdcc40bdca2fae23449c77ef4
myl <myl@dcc22606-d844-4505-a083-a3dbd7937ca2>
2010-01-12 13:39:47 +0000

    source folder
    git-svn-id: http://svn.lirec.eu@277 dcc22606-d844-4505-a083-a3dbd7937ca2

---
+++ AgentMind/trunk/AgentMind/WorldTest/src/sObject.java
@@ -0,0 +1,87 @@
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.ListIterator;
+import java.util.StringTokenizer;
+
+/*
+ * Created on 4/Fev/2006
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+
+/**
+ * @author João Dias
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class sObject {
+	
+	private static int buffsize = 250;
+	
+	private ArrayList _properties;
+	private String _objectName;
+	
+	public sObject(String name) {
+		_properties = new ArrayList();
+		_objectName = name;
+	}
+	
+	public String Name() {
+		return _objectName;
+	}
+	
+	public void AddProperty(Property p) {
+		_properties.add(p);
+	}
+	
+	public String GetPropertiesList() {
+		String properties = "";
+		Property p;
+		
+		ListIterator li = _properties.listIterator();
+		while(li.hasNext()) {
+			p = (Property) li.next();
+			properties = properties + p.GetName() + ":" + p.GetValue() + " "; 
+		}
+		
+		return properties;
+	}
+	
+	public static sObject ParseFile(String name) {
+		sObject obj = new sObject(name);
+		byte[] buffer = new byte[buffsize];
+		String data = "";
+		int readCharacters;
+		
+		try {
+			FileInputStream f = new FileInputStream(name + ".txt");
+			while((readCharacters=f.read(buffer))>0) {
+				data = data + new String(buffer,0,readCharacters);
+			}
+			ParseData(obj, data);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return obj;
+	}
+	
+	private static void ParseData(sObject obj, String data) {
+		StringTokenizer st;
+		
+		st = new StringTokenizer(data,"\r\n");
+		while(st.hasMoreTokens()) {
+			ParseLine(obj, st.nextToken());
+		}
+	}
+	
+	private static void ParseLine(sObject obj, String line) {
+		StringTokenizer st;
+		
+		st = new StringTokenizer(line," ");
+		obj.AddProperty(new Property(st.nextToken(),st.nextToken()));
+	}
+}
