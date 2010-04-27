cf1682ca0febc2aa5a414693186feb914d2e1519
joao.dias <joao.dias@dcc22606-d844-4505-a083-a3dbd7937ca2>
2010-04-27 14:38:25 +0000

    git-svn-id: http://svn.lirec.eu@398 dcc22606-d844-4505-a083-a3dbd7937ca2

--- AgentMind/trunk/AgentMind/AgentLauncher/src/AgentLauncher.java
+++ AgentMind/trunk/AgentMind/AgentLauncher/src/AgentLauncher.java
@@ -31,6 +31,7 @@ public class AgentLauncher {
 		short agentPlatform = 0;
 		String platform;
 		Agent agent = null;
+		String args2[] = null;
 		
 		
 		if(args.length == 0){
@@ -39,15 +40,15 @@ public class AgentLauncher {
 		}
 		
 		//Load the arguments from the scenario definition present in scenarios.xml	
-		if(args.length == 2){
+		if(args.length >= 2){
 			ScenarioLoaderHandler scenHandler = new ScenarioLoaderHandler(args[0],args[1]);
 			SAXParserFactory factory = SAXParserFactory.newInstance();
 			SAXParser parser = factory.newSAXParser();
 			parser.parse(new File(Agent.MIND_PATH + "LirecScenarios.xml"), scenHandler);
-			args = scenHandler.getAgentArguments();
+			args2 = scenHandler.getAgentArguments();
 		}
 		
-		platform = args[0];
+		platform = args2[0];
 		
 		if(platform.equalsIgnoreCase("ION")){
 			agentPlatform = AgentPlatform.ION;
@@ -60,14 +61,14 @@ public class AgentLauncher {
 		
 		switch(agentPlatform){
 			case AgentPlatform.ION:
-				if(args.length == 14){
+				if(args2.length >= 11){
 					
-					agent = new Agent(agentPlatform,args[1], Integer.parseInt(args[2]), args[3],Boolean.parseBoolean(args[10]), args[4], args[5], args[6], args[7], args[8], args[9],args[11],args[12],args[13],null,null);
+					agent = new Agent(agentPlatform,args2[1], Integer.parseInt(args[2]),"", Boolean.parseBoolean(args2[3]), args2[4], args2[5], args2[6], args2[7], args2[8], args2[9], args2[10],null,null);
 				}
-				else if(args.length == 5)
+				else if(args2.length == 5)
 				{
 					System.err.println("Creating the agent instance");
-					agent = new Agent(agentPlatform,args[1],Integer.parseInt(args[2]), args[3], args[4]);
+					agent = new Agent(agentPlatform,args2[1],Integer.parseInt(args2[2]), args2[3], args2[4]);
 				}
 				else
 				{
@@ -77,13 +78,13 @@ public class AgentLauncher {
 				
 			case AgentPlatform.WORLDSIM:
 				String saveDirectory = "";
-				if (args.length == 4){
-					agent = new Agent(agentPlatform, args[1],Integer.parseInt(args[2]),saveDirectory,args[3]);
-				}else if(args.length >= 11){
+				if (args2.length == 4){
+					agent = new Agent(agentPlatform, args2[1],Integer.parseInt(args2[2]),saveDirectory,args2[3]);
+				}else if(args2.length >= 11){
 					HashMap<String,String> properties = new HashMap<String,String>();
 					ArrayList<String> goals = new ArrayList<String>();
 					readPropertiesAndGoals(args, properties, goals);
-					agent = new Agent(agentPlatform,args[1], Integer.parseInt(args[2]),saveDirectory,Boolean.parseBoolean(args[3]),args[4],null,null, args[5], args[6], args[7],args[8],args[9],args[10], properties, goals);		
+					agent = new Agent(agentPlatform,args2[1], Integer.parseInt(args2[2]),saveDirectory,Boolean.parseBoolean(args2[3]),args2[4], args2[5], args2[6], args2[7],args2[8],args2[9],args2[10], properties, goals);		
 				}else{
 					System.err.println("Wrong number of arguments!");
 				}
