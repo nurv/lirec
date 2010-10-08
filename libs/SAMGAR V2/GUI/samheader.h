6dbd5e65ff71f24fe04d1da20c7ac394504889ae
KDucasse <KDucasse@dcc22606-d844-4505-a083-a3dbd7937ca2>
2010-10-08 12:34:50 +0000

    git-svn-id: http://svn.lirec.eu@507 dcc22606-d844-4505-a083-a3dbd7937ca2

---
+++ libs/SAMGAR V2/GUI/samheader.h
@@ -0,0 +1,64 @@
+
+#include "server3Gui.h"
+
+//#define maxmodules 50
+//#define maxconns   100
+//#define maxports   10
+
+
+int Connecting=0,Currentconntype=0,numofconnections=0;
+double connstartx=0,connstarty=0,Xshift=0;
+ SamConnection SamCons[100];
+ SamConnection TempConnect;
+
+ MyConnectionView *myconnwindow;
+
+class DataPort : public BufferedPort<Bottle> 
+{
+	 
+     virtual void onRead(Bottle& b) // will it wait for this one to finish before calling it again?
+	 {
+		 string mystring = b.get(0).asString().c_str();
+		 if(mystring.compare("Add_Module")==0)
+		 {
+			 string portnames[maxports];
+			 string name=b.get(1).asString().c_str();
+			 for(int uu = 2;uu<b.size();uu++)
+			 {
+				 portnames[uu-2]=b.get(uu).asString().c_str();
+			 }
+			 myconnwindow->AddAlterModule(SAMdef::add_module,name,portnames,0,0,true);
+		 }
+		 else if(mystring.compare("Active_connection")==0)
+		 {
+			 myconnwindow->mylogwindow->WriteToLog("in active connection",true);
+			 string fake;
+			 string fake10[maxports];
+			 myconnwindow->AddAlterModule(SAMdef::online_connection,fake,fake10,0,0,false,b.get(1).asString().c_str(),b.get(2).asString().c_str());			
+		 }
+		else if(mystring.compare("Disactive_connection")==0)
+		 {
+
+			 string fake;
+			 string fake10[maxports];
+			 myconnwindow->AddAlterModule(SAMdef::offline_connection,fake,fake10,0,0,false,b.get(1).asString().c_str(),b.get(2).asString().c_str());			
+		 }
+     }
+};
+class myPortReport:public PortReport
+{
+ void report(const PortInfo& info)
+ {
+	if(info.created)
+	{
+	printf("found con %s \n",info.sourceName.c_str());
+	myconnwindow->AddAlterModule(SAMdef::online_module,info.sourceName.c_str());
+	}
+	else
+	{
+	printf("lost con %s \n",info.sourceName.c_str());
+	myconnwindow->AddAlterModule(SAMdef::offline_module,info.sourceName.c_str());
+	}
+
+ }
+};
