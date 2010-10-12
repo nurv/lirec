d620eadc732f54150aabf6a5640f2ce0b8c8522a
KDucasse <KDucasse@dcc22606-d844-4505-a083-a3dbd7937ca2>
2010-10-12 12:53:03 +0000

    git-svn-id: http://svn.lirec.eu@519 dcc22606-d844-4505-a083-a3dbd7937ca2

---
+++ libs/SAMGAR V2/Module/SamExample.cpp
@@ -0,0 +1,28 @@
+
+
+
+#include "ExampleOne.h"
+
+
+
+int main(void) 
+{
+	ExampleOneWrite myfirstmodule;
+	myfirstmodule.SamInit();
+
+	ExampleTwoRead mysecondmodule;
+	mysecondmodule.SamInit();
+
+
+	while(1)
+	{
+		myfirstmodule.SamIter();
+		mysecondmodule.SamIter();
+		yarp::os::Time::delay(2);
+	}
+
+  return 0;
+}
+
+
+	
