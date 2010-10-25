2914dd948a5c9806222b5a9bf3ce546bdb94d838
joao.dias <joao.dias@dcc22606-d844-4505-a083-a3dbd7937ca2>
2010-10-25 12:32:40 +0000

    Integrating final ToM into main version
    git-svn-id: http://svn.lirec.eu@587 dcc22606-d844-4505-a083-a3dbd7937ca2

--- AgentMind/trunk/AgentMind/FAtiMA/src/FAtiMA/util/Constants.java
+++ AgentMind/trunk/AgentMind/FAtiMA/src/FAtiMA/util/Constants.java
@@ -1,7 +1,10 @@
 package FAtiMA.util;
 
+import FAtiMA.wellFormedNames.*;
+
 public abstract class Constants {
 
 	public static final String SELF = "SELF";
 	public static final String EMPTY_LOCATION = "";
+	public static final Symbol UNIVERSAL = new Symbol("*");
 }
