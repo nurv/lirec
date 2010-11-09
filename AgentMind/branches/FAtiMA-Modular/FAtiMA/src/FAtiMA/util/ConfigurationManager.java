93731e9edaab020c50058a2d5f60dd1a40f41032
joao.dias <joao.dias@dcc22606-d844-4505-a083-a3dbd7937ca2>
2010-11-09 11:07:32 +0000

    git-svn-id: http://svn.lirec.eu@663 dcc22606-d844-4505-a083-a3dbd7937ca2

---
+++ AgentMind/branches/FAtiMA-Modular/FAtiMA/src/FAtiMA/util/ConfigurationManager.java
@@ -0,0 +1,143 @@
+package FAtiMA.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.SAXException;
+
+import FAtiMA.AgentCore;
+import FAtiMA.util.enumerables.AgentPlatform;
+import FAtiMA.util.parsers.ScenarioLoaderHandler;
+
+/*
+*  
+* A read-only singleton used to get the path of the configuration files 
+* author: samuel
+*
+*/
+public class ConfigurationManager {
+	private static final String PLATFORM = "platform";
+	private static final String SAVE_DIRECTORY = "saveDirectory";
+	private static final String HOST = "host";
+	private static final String PORT = "port";
+	private static final String DISPLAY_MODE = "displayMode";
+	private static final String NAME = "name";
+	private static final String DISPLAY_NAME = "displayName";	
+	private static final String SEX = "sex";
+	private static final String ROLE = "role";
+	private static final String ACTIONS_FILE = "actionsFile";	
+	private static final String GOALS_FILE = "goalsFile";
+	
+	
+	private String mindPath;
+	private HashMap<String,String> agentConfiguration;
+	private HashMap<String,String> agentProperties;
+	
+	//private String saveDirectory, actionsFile, goalsFile, personalityFile;
+	
+	//singleton pattern
+	private ConfigurationManager(){}
+	private static ConfigurationManager soleInstance = new ConfigurationManager();
+	
+
+	private static ConfigurationManager getInstance(){
+		return soleInstance;
+	}
+	
+	public static void initialize(String mindPath, String scenarioName, String agentName) throws SAXException, IOException, ParserConfigurationException{
+		String scenarioFileName = mindPath + AgentCore.SCENARIO_FILENAME;	
+		getInstance().mindPath = mindPath;
+		loadConfigurationFromScenarioFile(scenarioFileName,scenarioName,agentName);
+	}
+	
+	private static void loadConfigurationFromScenarioFile(String scenarioFileName, String scenarioName, String agentName) throws SAXException, IOException, ParserConfigurationException{
+		ScenarioLoaderHandler scenHandler = new ScenarioLoaderHandler(scenarioName,agentName);
+		SAXParserFactory factory = SAXParserFactory.newInstance();
+		SAXParser parser = factory.newSAXParser();
+		parser.parse(new File(scenarioFileName), scenHandler);
+		//side-effects
+		getInstance().agentProperties = scenHandler.getAgentProperties();
+		getInstance().agentConfiguration = scenHandler.getAgentConfiguration();
+	}
+	
+	public static short getPlatform(){
+		String platform = getInstance().getConfigurationValue(PLATFORM);
+		if(platform.equalsIgnoreCase("ION")){
+			return AgentPlatform.ION;
+		}else if(platform.equalsIgnoreCase("WORLDSIM")){
+			return AgentPlatform.WORLDSIM;
+		}else{
+			return -1; //invalid platform
+		}
+	}
+	
+	public static String getSaveDirectory(){
+		return getInstance().getConfigurationValue(SAVE_DIRECTORY);
+	}
+		
+	public static String getHost(){
+		return getInstance().getConfigurationValue(HOST);		
+	}
+	
+	public static int getPort(){
+		return Integer.parseInt(getInstance().getConfigurationValue(PORT));
+	}
+	
+	public static String getActionsFile(){
+		String actionsFile = getInstance().getConfigurationValue(ACTIONS_FILE);		
+
+		return getInstance().mindPath + actionsFile + ".xml";
+	}
+	
+	public static String getGoalsFile(){
+		String goalsFile = getInstance().getConfigurationValue(GOALS_FILE);		
+		return getInstance().mindPath + goalsFile + ".xml";
+	}
+	
+	public static String getRole(){ 
+		return getInstance().getConfigurationValue(ROLE);	
+	}
+	
+	public static String getSex(){ 
+		return getInstance().getConfigurationValue(SEX);	
+	}
+	
+	public static String getName(){ 
+		return getInstance().getConfigurationValue(NAME);	
+	}
+	
+	public static String getDisplayName(){ 
+		return getInstance().getConfigurationValue(DISPLAY_NAME);	
+	}
+	
+	public static boolean getDisplayMode(){ 
+		return Boolean.parseBoolean(getInstance().getConfigurationValue(DISPLAY_MODE));	
+	}
+	
+	public static String getPersonalityFile(){
+		return getInstance().mindPath + "roles/" + getRole() + "/" + getRole() + ".xml";
+	}
+
+	public static HashMap<String,String> getAgentProperties(){
+		return getInstance().agentProperties; 
+	}
+	
+	
+	//Auxiliary Method To Prevent From Getting Inexistent Configurations
+	private String getConfigurationValue(String name){
+		if(this.agentConfiguration.containsKey(name)){
+			return this.agentConfiguration.get(name);
+		}else{
+			throw new RuntimeException("There is no configuration with such name: " + name);
+		}
+	}
+	
+	
+	
+}
--- AgentMind/branches/FAtiMA-Modular/FAtiMA/src/FAtiMA/util/parsers/ScenarioLoaderHandler.java
+++ AgentMind/branches/FAtiMA-Modular/FAtiMA/src/FAtiMA/util/parsers/ScenarioLoaderHandler.java
@@ -1,6 +1,7 @@
 package FAtiMA.util.parsers;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 
 import org.xml.sax.Attributes;
@@ -14,41 +15,33 @@ public class ScenarioLoaderHandler extends ReflectXMLHandler{
 	private boolean _isPretendedScenario;
 	private boolean _isPretendedCharacter;
 	private ArrayList<String> _worldSimArgs;
-	private ArrayList<String> _agentArgs;
+	private HashMap<String,String> _agentProperties;	
+	private HashMap<String,String> _agentConfiguration;
+	
 	
 	public ScenarioLoaderHandler(String scenarioName){
 		this._scenarioName = scenarioName;
 		this._characterName = "";
 		this._worldSimArgs = new ArrayList<String>();
-		this._agentArgs = new ArrayList<String>();
+		this._agentConfiguration = new HashMap<String,String>();
 		this._isPretendedScenario = false;	
 	}	
 	
 	public ScenarioLoaderHandler(String scenarioName, String characterName){
 		this._scenarioName = scenarioName;
 		this._characterName = characterName;
-		this._agentArgs = new ArrayList<String>();
+		this._agentConfiguration = new HashMap<String,String>();
+		this._agentProperties = new HashMap<String,String>();
 		this._worldSimArgs = new ArrayList<String>();
+		this._agentConfiguration.put("saveDirectory", ""); //default value
 		this._isPretendedScenario = false;
 		this._isPretendedCharacter = false;
+		
 	}
 	
 	public String[] getWorldSimArguments(){
 		String[] result = new String[_worldSimArgs.size()];		
 		Iterator<String> it = _worldSimArgs.iterator();
-		int i = 0;
-		
-		while(it.hasNext()){
-			String arg = it.next();
-			result[i] = arg;
-			i++;
-		}
-		return result;
-	}
-	
-	public String[] getAgentArguments(){
-		String[] result = new String[_agentArgs.size()];		
-		Iterator<String> it = _agentArgs.iterator();
 		int i = 0;
 		
 		while(it.hasNext()){
@@ -90,26 +83,9 @@ public class ScenarioLoaderHandler extends ReflectXMLHandler{
 		if(_isPretendedScenario){
 			if(_characterName.equalsIgnoreCase(attributes.getValue("name"))){
 				this._isPretendedCharacter = true;
-				_agentArgs.add(attributes.getValue("platform"));
-				_agentArgs.add(attributes.getValue("host"));
-				_agentArgs.add(attributes.getValue("port"));
-				_agentArgs.add(attributes.getValue("saveDirectory"));
-				_agentArgs.add(attributes.getValue("name"));
-				_agentArgs.add(attributes.getValue("displayMode"));
-				_agentArgs.add(attributes.getValue("sex"));
-				_agentArgs.add(attributes.getValue("role"));
-				_agentArgs.add(attributes.getValue("displayName"));
-				_agentArgs.add(attributes.getValue("actionsFile"));
-				_agentArgs.add(attributes.getValue("goalsFile"));
-				_agentArgs.add(attributes.getValue("cultureName"));
-				if(attributes.getValue("load") == null) 
-				{
-					
-					_agentArgs.add("false");
-				}
-				else
-				{
-					_agentArgs.add(attributes.getValue("load"));
+				
+				for(int i=0; i < attributes.getLength(); i++ ){
+					_agentConfiguration.put(attributes.getLocalName(i), attributes.getValue(i));
 				}
 			}else{
 				this._isPretendedCharacter = false;
@@ -119,8 +95,16 @@ public class ScenarioLoaderHandler extends ReflectXMLHandler{
 	
 	public void Property(Attributes attributes){
 		if(_isPretendedScenario && _isPretendedCharacter){
-			_agentArgs.add(attributes.getValue("name")+":"+attributes.getValue("value"));
+			
+			_agentProperties.put(attributes.getValue("name"),attributes.getValue("value"));
 		}
 	}
 	
+	public  HashMap<String,String> getAgentProperties(){
+		return this._agentProperties;
+	}
+	
+	public HashMap<String, String> getAgentConfiguration(){
+		return this._agentConfiguration;
+	}
 }
