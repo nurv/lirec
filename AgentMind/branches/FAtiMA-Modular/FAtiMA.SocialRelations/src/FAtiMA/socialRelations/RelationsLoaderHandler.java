b6987d6149a18e88d9cfbcfadb46e38476f2c481
joao.dias <joao.dias@dcc22606-d844-4505-a083-a3dbd7937ca2>
2010-11-16 00:22:38 +0000

    git-svn-id: http://svn.lirec.eu@719 dcc22606-d844-4505-a083-a3dbd7937ca2

--- AgentMind/branches/FAtiMA-Modular/FAtiMA.SocialRelations/src/FAtiMA/socialRelations/RelationsLoaderHandler.java
+++ AgentMind/branches/FAtiMA-Modular/FAtiMA.SocialRelations/src/FAtiMA/socialRelations/RelationsLoaderHandler.java
@@ -2,30 +2,42 @@ package FAtiMA.socialRelations;
 
 import org.xml.sax.Attributes;
 
-import FAtiMA.Core.AgentCore;
+import FAtiMA.Core.AgentModel;
+import FAtiMA.Core.deliberativeLayer.goals.Goal;
 import FAtiMA.Core.deliberativeLayer.plan.Effect;
+import FAtiMA.Core.deliberativeLayer.plan.Step;
 import FAtiMA.Core.exceptions.ContextParsingException;
 import FAtiMA.Core.exceptions.InvalidEmotionTypeException;
 import FAtiMA.Core.util.Constants;
-import FAtiMA.Core.util.parsers.LikeCondition;
 import FAtiMA.Core.util.parsers.ReflectXMLHandler;
+import FAtiMA.Core.wellFormedNames.Name;
+import FAtiMA.Core.wellFormedNames.Substitution;
+import FAtiMA.Core.wellFormedNames.Symbol;
 
 public class RelationsLoaderHandler  extends ReflectXMLHandler{
-	
-	private AgentCore _ag;
-	
-	public RelationsLoaderHandler(AgentCore ag)
-	{
-		this._ag = ag;
-	}
 
+	private String _currentGoalKey;
+	private String _currentActionKey;
+	private String _conditionType;
+	private boolean _precondition;
+	private AgentModel _aM;
+	private float _probability;
+    private Substitution _self = new Substitution(new Symbol("[SELF]"), new Symbol(Constants.SELF));
+    
+	
+	
+	public RelationsLoaderHandler(AgentModel aM){
+		_aM = aM;
+	}
+	
+	
 	//parsed from the role file
 	public void Relation(Attributes attributes)
 	{
 		float respect;
 		String target = attributes.getValue("target");
 		float like = Float.parseFloat(attributes.getValue("like"));
-		LikeRelation.getRelation(Constants.SELF, target).setValue(_ag.getMemory(),like);
+		LikeRelation.getRelation(Constants.SELF, target).setValue(_aM.getMemory(),like);
 
 		String auxRespect = attributes.getValue("respect");
 		if(auxRespect == null)
@@ -36,21 +48,23 @@ public class RelationsLoaderHandler  extends ReflectXMLHandler{
 		{
 			respect = Float.parseFloat(auxRespect);
 		}
-		RespectRelation.getRelation(Constants.SELF, target).setValue(_ag.getMemory(),respect);
+		RespectRelation.getRelation(Constants.SELF, target).setValue(_aM.getMemory(),respect);
 	}
-
+	
+	
 
 	//used in goals
 	public void LikeRelation(Attributes attributes)
 	{
 		LikeCondition lc;
-
+		Goal g;
+		
 		try
 		{
 			lc = LikeCondition.ParseSocialCondition(attributes);
-
 			lc.MakeGround(_self);
-			_currentGoal.AddCondition(_conditionType, lc);
+			g = _aM.getDeliberativeLayer().getGoalLibrary().GetGoal(Name.ParseName(_currentGoalKey));
+			g.AddCondition(_conditionType, lc);
 		}
 		catch(Exception e)
 		{
@@ -62,16 +76,49 @@ public class RelationsLoaderHandler  extends ReflectXMLHandler{
 	//used in actions
 	public void LikeCondition(Attributes attributes) throws InvalidEmotionTypeException, ContextParsingException {
 		LikeCondition l;
-
 		l = LikeCondition.ParseSocialCondition(attributes);
-		//l.MakeGround(_self);
+		l.MakeGround(_self);
 
-		if(_precondition) 
-			_currentOperator.AddPrecondition(l);
+		Step action = _aM.getDeliberativeLayer().getEmotionalPlanner().GetStep(Name.ParseName(_currentActionKey));
+		if(_precondition)
+			action.AddPrecondition(l);
 		else {
-			String operatorName = _currentOperator.getName().GetFirstLiteral().toString();
-			_currentOperator.AddEffect(new Effect(_am, operatorName,_probability, l));	
+			String operatorName = action.getName().GetFirstLiteral().toString();
+			action.AddEffect(new Effect(_aM, operatorName,_probability, l));	
 		}
 	}
+	
+	
+
+	public void ActivePursuitGoal(Attributes attributes) {
+    	_currentGoalKey = attributes.getValue("name");
+    }
+	
+	public void Action(Attributes attributes){
+	 	_currentActionKey = attributes.getValue("name");	
+	}
+	
+	public void SuccessConditions(Attributes attributes) {
+	    _conditionType = "SuccessConditions";
+	}
+	    
+	 public void FailureConditions(Attributes attributes) {
+	    _conditionType = "FailureConditions";
+	 }
+	 
+	 public void PreConditions(Attributes attributes) {
+		 _conditionType = "PreConditions";
+		 _precondition = true;
+	 }
+	 
+	 public void Effects(Attributes attributes) {
+	    _precondition = false;
+	}
+	 
+	 public void Effect(Attributes attributes) {
+		_probability = Float.parseFloat(attributes.getValue("probability"));
+	 }
+		
+	 
 }
 
