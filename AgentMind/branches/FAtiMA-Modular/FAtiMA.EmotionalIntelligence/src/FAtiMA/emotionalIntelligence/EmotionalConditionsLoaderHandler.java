959e9bd1c64fb3b1bc8627c993cb296ba99ed9f6
joao.dias <joao.dias@dcc22606-d844-4505-a083-a3dbd7937ca2>
2011-01-11 21:04:38 +0000

    git-svn-id: http://svn.lirec.eu@805 dcc22606-d844-4505-a083-a3dbd7937ca2

---
+++ AgentMind/branches/FAtiMA-Modular/FAtiMA.EmotionalIntelligence/src/FAtiMA/emotionalIntelligence/EmotionalConditionsLoaderHandler.java
@@ -0,0 +1,58 @@
+package FAtiMA.emotionalIntelligence;
+
+import org.xml.sax.Attributes;
+
+import FAtiMA.Core.AgentModel;
+import FAtiMA.Core.deliberativeLayer.goals.Goal;
+import FAtiMA.Core.util.Constants;
+import FAtiMA.Core.util.parsers.ReflectXMLHandler;
+import FAtiMA.Core.wellFormedNames.Name;
+import FAtiMA.Core.wellFormedNames.Substitution;
+import FAtiMA.Core.wellFormedNames.Symbol;
+
+public class EmotionalConditionsLoaderHandler extends ReflectXMLHandler {
+	
+	private AgentModel _aM;
+	private String _currentGoalKey;
+	private String _conditionType;
+	private Substitution _self = new Substitution(new Symbol("[SELF]"), new Symbol(Constants.SELF));
+	
+	public EmotionalConditionsLoaderHandler(AgentModel aM){
+		_aM = aM;
+	}
+	
+	public void ActivePursuitGoal(Attributes attributes) {
+    	_currentGoalKey = attributes.getValue("name");
+    }
+	 
+	public void EmotionalEpisodeCondition(Attributes attributes)
+	{
+		EmotionalEpisodeCondition ee;
+		Goal g;
+		 
+		try
+		{
+			ee = EmotionalEpisodeCondition.ParseEmotionalEpisodeCondition(attributes);
+			ee.MakeGround(_self);
+			g = _aM.getDeliberativeLayer().getGoalLibrary().GetGoal(Name.ParseName(_currentGoalKey));
+			g.AddCondition(_conditionType, ee);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	 
+	public void FailureConditions(Attributes attributes) {
+	   _conditionType = "FailureConditions";
+	}
+	 
+	public void PreConditions(Attributes attributes) {
+		_conditionType = "PreConditions";
+	}
+	 
+	public void SuccessConditions(Attributes attributes) {
+	   _conditionType = "SuccessConditions";
+	}
+
+}
