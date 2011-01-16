12b8a3aea412faf6662e92cce2b120c8214ab122
KDucasse <KDucasse@dcc22606-d844-4505-a083-a3dbd7937ca2>
2011-01-16 12:52:22 +0000

    1.Added autoload function for connections, if a file in the executable folder exists under the name "AutoLoad.SAMstate" it will automatically load on startup.
    2.Cleaned up the multiple connections, so it runs slightly faster.
    3.fixed bug when loading files, now shows possible files to load.
    4.clears previous connections when loading samgar gui, so old project connections cease to exist.
    
    git-svn-id: http://svn.lirec.eu@835 dcc22606-d844-4505-a083-a3dbd7937ca2

---
+++ libs/SAMGAR/trunk/GUI/samheader.h
@@ -0,0 +1,88 @@
+#ifndef _SAMGARHEADER_H
+#define _SAMGARHEADER_H
+/** \file samheader.h
+ *
+ *  This file contains data for SAMGAR data port and report port.
+ *
+ *  TODO: I think that code should be separated from the header. And therefore
+ *  file name should be changed.
+ */
+
+#include "server3Gui.h"
+
+MyConnectionView *myconnwindow;
+
+/** \class DataPort
+ *
+ *  TODO: Put here description what does this class do.
+ */
+class DataPort : public BufferedPort<Bottle> 
+{   
+BufferedPort<Bottle> Repeater;
+
+public:
+	DataPort()
+	{
+    Repeater.open("/Repeater");
+	}
+
+   virtual void onRead(Bottle& b) // will it wait for this one to finish before calling it again?
+   {
+	  Bottle& outBot1 = Repeater.prepare();
+	  outBot1 = b;
+	  Repeater.write();
+      string mystring = b.get(0).asString().c_str();
+      if(mystring.compare("Add_Module")==0)
+      {
+	 string portnames[maxports];
+	 string name=b.get(1).asString().c_str();
+	 for(int uu = 2;uu<b.size();uu++)
+	 {
+	    portnames[uu-2]=b.get(uu).asString().c_str();
+	 }
+	 myconnwindow->AddAlterModule(SAMdef::add_module, name,
+				      portnames, 0, 0, true);
+      }
+      else if(mystring.compare("Active_connection")==0)
+      {
+	 myconnwindow->mylogwindow->WriteToLog("in active connection",true);
+	 string fake;
+	 string fake10[maxports];
+	 myconnwindow->AddAlterModule(SAMdef::online_connection, fake, fake10,
+				      0, 0, false,b.get(1).asString().c_str(),
+				      b.get(2).asString().c_str());			
+      }
+      else if(mystring.compare("Disactive_connection")==0)
+      {
+	 string fake;
+	 string fake10[maxports];
+	 myconnwindow->AddAlterModule(SAMdef::offline_connection, fake, fake10,
+				      0, 0, false,b.get(1).asString().c_str(),
+				      b.get(2).asString().c_str());			
+      }
+   }
+};
+
+/** \class myPortReport
+ *
+ *  TODO: Put here description what does this class do.
+ */
+class myPortReport:public PortReport
+{
+   void report(const PortInfo& info)
+   {
+      if(info.created)
+      {
+	 printf("found con %s \n",info.sourceName.c_str());
+	 myconnwindow->AddAlterModule(SAMdef::online_module,info.sourceName.c_str());
+      }
+      else
+      {
+	 printf("lost con %s \n",info.sourceName.c_str());
+	 myconnwindow->AddAlterModule(SAMdef::offline_module,info.sourceName.c_str());
+      }
+
+   }
+};
+
+#endif 
