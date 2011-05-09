e3b39ccb499afc19e774ac6b0bc44e331474415a
dave <dave@dcc22606-d844-4505-a083-a3dbd7937ca2>
2011-05-09 09:58:31 +0000

    added facebook js code
    git-svn-id: http://svn.lirec.eu@1110 dcc22606-d844-4505-a083-a3dbd7937ca2

---
+++ scenarios/GerminationX/fungi/js/game.js
@@ -0,0 +1,72 @@
+// GerminationX Copyright (C) 2011 FoAM vzw    \_\ __     /\
+//                                          /\    /_/    / /  
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+var facebook_enabled=true;
+
+// add some html to the page
+function print(dst,text)
+{ 
+    var div=document.createElement("div");   
+    div.id = "";
+    div.innerHTML = text;
+    document.getElementById(dst).appendChild(div);
+}
+
+function debug(text) { print("debug",text); }
+function facebook_status(text) { print("facebook_status",text); }
+
+//////////////////////////////////////////////////////////
+
+debug("hello");
+
+function game_world()
+{
+    this.player_name = "";
+    this.player_id = 0;
+
+    this.startup = function()
+    {
+        debug("starting up");
+        if (facebook_enabled)
+        {
+            // the API key is stored in the untracked api-key.js
+            // echo "var gx_api_key = '1234567890abcdef';" > api-key.js
+            this.fb = new fb_interface(gx_fb_key, this.fb_refresh);
+        }
+    }
+
+    this.fb_refresh = function(name,data)
+    {
+        if (name=="me")
+        {
+            var name = data.name; //.replace(/\s/g, "");
+            game.player_name=name;
+            game.player_id=data.id;
+        }
+    }
+}
+
+var game = new game_world();
+game.startup();
+
+function crankfb()
+{
+    game.fb.poll();
+    setTimeout("crankfb()",10); 
+}
+
+crankfb();
+
+
