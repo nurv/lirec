e475b75eea91dcea5adbb0db79ef51a75e51b812
muk7 <muk7@dcc22606-d844-4505-a083-a3dbd7937ca2>
2011-07-19 17:05:01 +0000

    start a new episode after forgetting
    git-svn-id: http://svn.lirec.eu@1196 dcc22606-d844-4505-a083-a3dbd7937ca2

--- AgentMind/branches/FAtiMA-Modular/FAtiMA.AdvancedMemory/src/main/java/FAtiMA/advancedMemoryComponent/AdvancedMemoryTask.java
+++ AgentMind/branches/FAtiMA-Modular/FAtiMA.AdvancedMemory/src/main/java/FAtiMA/advancedMemoryComponent/AdvancedMemoryTask.java
@@ -1,3 +1,29 @@
+/** 
+ * AdvancedMemoryTask.java - TimerTask for Advanced Memory  
+ * 
+ * Copyright (C) 2006 GAIPS/INESC-ID 
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ * Company: HWU
+ * Project: LIREC
+ * Created: 05/07/2011
+ * @author: Matthias Keysermann
+ * Email to: muk7@hw.ac.uk
+ */
+
 package FAtiMA.advancedMemoryComponent;
 
 import java.text.SimpleDateFormat;
@@ -21,13 +47,16 @@ public class AdvancedMemoryTask extends TimerTask {
 
 	public void run() {
 
+		// set date format
 		Calendar cal = Calendar.getInstance();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
 		String strTime = sdf.format(cal.getTime());
 
+		// initialise
 		ArrayList<String> gAttributes;
 		AdvancedMemoryWriter advancedMemoryWriter;
 
+		// generalise
 		System.out.println(strTime + ": performing Generalisation");
 		gAttributes = new ArrayList<String>();
 		gAttributes.add("subject");
@@ -38,6 +67,7 @@ public class AdvancedMemoryTask extends TimerTask {
 		System.out.println(strTime + ": saving XML Advanced Memory");
 		advancedMemoryWriter.outputGERtoXML(filepath + "XMLMemory_" + strTime + "_Advanced_SubActTar" + ".xml");
 
+		// generalise
 		System.out.println(strTime + ": performing Generalisation");
 		gAttributes = new ArrayList<String>();
 		gAttributes.add("subject");
@@ -48,6 +78,7 @@ public class AdvancedMemoryTask extends TimerTask {
 		System.out.println(strTime + ": saving XML Advanced Memory");
 		advancedMemoryWriter.outputGERtoXML(filepath + "XMLMemory_" + strTime + "_Advanced_SubActLoc" + ".xml");
 
+		// generalise
 		System.out.println(strTime + ": performing Generalisation");
 		gAttributes = new ArrayList<String>();
 		gAttributes.add("subject");
@@ -58,6 +89,7 @@ public class AdvancedMemoryTask extends TimerTask {
 		System.out.println(strTime + ": saving XML Advanced Memory");
 		advancedMemoryWriter.outputGERtoXML(filepath + "XMLMemory_" + strTime + "_Advanced_SubActTim" + ".xml");
 
+		// generalise
 		System.out.println(strTime + ": performing Generalisation");
 		gAttributes = new ArrayList<String>();
 		gAttributes.add("subject");
@@ -70,6 +102,7 @@ public class AdvancedMemoryTask extends TimerTask {
 		System.out.println(strTime + ": saving XML Advanced Memory");
 		advancedMemoryWriter.outputGERtoXML(filepath + "XMLMemory_" + strTime + "_Advanced_SubActTarLocTim" + ".xml");
 
+		// generalise
 		System.out.println(strTime + ": performing Generalisation");
 		gAttributes = new ArrayList<String>();
 		gAttributes.add("subject");
--- AgentMind/branches/FAtiMA-Modular/FAtiMA/src/FAtiMA/Core/memory/episodicMemory/EpisodicMemoryTask.java
+++ AgentMind/branches/FAtiMA-Modular/FAtiMA/src/FAtiMA/Core/memory/episodicMemory/EpisodicMemoryTask.java
@@ -1,3 +1,30 @@
+/** 
+ * EpisodicMemoryTask.java - TimerTask for Episodic Memory  
+ * 
+ * Copyright (C) 2006 GAIPS/INESC-ID 
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ * Company: HWU
+ * Project: LIREC
+ * Created: 05/07/2011
+ * @author: Matthias Keysermann
+ * Email to: muk7@hw.ac.uk
+ */
+
+
 package FAtiMA.Core.memory.episodicMemory;
 
 import java.text.SimpleDateFormat;
@@ -20,14 +47,20 @@ public class EpisodicMemoryTask extends TimerTask {
 
 	public void run() {
 
+		// move events to LTM
+		memory.getEpisodicMemory().MoveSTEMtoAM();
+		
+		// set date format
 		Calendar cal = Calendar.getInstance();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
 		String strTime = sdf.format(cal.getTime());
 
+		// XML Memory export
 		System.out.println(strTime + ": saving XML Memory");
 		MemoryWriter memoryWriter = new MemoryWriter(memory);
 		memoryWriter.outputMemorytoXML(filepath + "XMLMemory_" + strTime + ".xml");
 
+		// Activation-Based Forgetting
 		System.out.println(strTime + ": performing AB Forgetting (20%)");
 		memory.getEpisodicMemory().calculateActivationValues();
 		ArrayList<ActionDetail> selected = memory.getEpisodicMemory().activationBasedSelectionByAmount(0.8);
@@ -37,9 +70,13 @@ public class EpisodicMemoryTask extends TimerTask {
 		}
 		memory.getEpisodicMemory().activationBasedForgetting(selectedIDs);
 
+		// XML Memory export
 		System.out.println(strTime + ": saving XML Memory");
 		memoryWriter.outputMemorytoXML(filepath + "XMLMemory_" + strTime + "_AfterABForgetting" + ".xml");
-
+		
+		// start a new episode
+		memory.getEpisodicMemory().StartEpisode(memory);
+		
 	}
 
 }
