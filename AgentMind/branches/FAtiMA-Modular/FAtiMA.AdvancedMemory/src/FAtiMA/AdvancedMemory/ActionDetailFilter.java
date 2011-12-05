15ee63910ce31dbc3efb22902ed5596f03fbd354
muk7 <muk7@dcc22606-d844-4505-a083-a3dbd7937ca2>
2011-12-05 20:24:03 +0000

    added action details filter
    git-svn-id: http://svn.lirec.eu@1426 dcc22606-d844-4505-a083-a3dbd7937ca2

---
+++ AgentMind/branches/FAtiMA-Modular/FAtiMA.AdvancedMemory/src/FAtiMA/AdvancedMemory/ActionDetailFilter.java
@@ -0,0 +1,101 @@
+/** 
+ * ActionDetailFilter.java - A class for filtering action details.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ * Company: HWU
+ * Project: LIREC
+ * Created: 05/12/11
+ * @author: Matthias Keysermann
+ * Email to: muk7@hw.ac.uk
+ * 
+ * History: 
+ * Matthias Keysermann: 05/12/11 - File created
+ * 
+ * **/
+
+package FAtiMA.AdvancedMemory;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+import edu.mit.jwi.item.IWord;
+
+import FAtiMA.AdvancedMemory.ontology.NounOntology;
+import FAtiMA.AdvancedMemory.ontology.TimeOntology;
+import FAtiMA.Core.memory.episodicMemory.ActionDetail;
+
+public class ActionDetailFilter implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	public static ArrayList<ActionDetail> filterActionDetails(ArrayList<ActionDetail> actionDetails, String attributeName, Object attributeValue, TimeOntology timeOntology,
+			NounOntology targetOntology, NounOntology objectOntology) {
+
+		ArrayList<ActionDetail> actionDetailsFiltered = new ArrayList<ActionDetail>();
+
+		for (ActionDetail actionDetail : actionDetails) {
+
+			AttributeItem attributeItem = new AttributeItem();
+			attributeItem.setName(attributeName);
+			attributeItem.setValue(actionDetail.getValueByName(attributeName), timeOntology);
+
+			Object attributeValueCurrent = attributeItem.getValue();
+
+			// check if value matches exactly
+			if (attributeValueCurrent != null && attributeValueCurrent.equals(attributeValue)) {
+				actionDetailsFiltered.add(actionDetail);
+
+			} else if (targetOntology != null && attributeName.equals("target")) {
+
+				// check if common hypernyms exists
+
+				String[] nouns = new String[2];
+				nouns[0] = String.valueOf(attributeValue);
+				nouns[1] = String.valueOf(attributeValueCurrent);
+
+				targetOntology.openDict();
+				LinkedList<IWord> nounsGeneralised = targetOntology.generaliseNouns(nouns);
+				targetOntology.closeDict();
+
+				if (nounsGeneralised.size() > 0) {
+					actionDetailsFiltered.add(actionDetail);
+				}
+
+			} else if (objectOntology != null && attributeName.equals("object")) {
+
+				// check if common hypernyms exists
+
+				String[] nouns = new String[2];
+				nouns[0] = String.valueOf(attributeValue);
+				nouns[1] = String.valueOf(attributeValueCurrent);
+
+				objectOntology.openDict();
+				LinkedList<IWord> nounsGeneralised = objectOntology.generaliseNouns(nouns);
+				objectOntology.closeDict();
+
+				if (nounsGeneralised.size() > 0) {
+					actionDetailsFiltered.add(actionDetail);
+				}
+
+			}
+
+		}
+
+		return actionDetailsFiltered;
+	}
+
+}
