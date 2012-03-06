571b20967379e90c6838048b0c11737e79923e0b
iwallace <iwallace@dcc22606-d844-4505-a083-a3dbd7937ca2>
2012-03-06 16:15:58 +0000

    up to 3 done, checked a bit
    git-svn-id: http://svn.lirec.eu@1537 dcc22606-d844-4505-a083-a3dbd7937ca2

--- scenarios/HWshared/HwuDialogSystem/examples/experimentDialog.java
+++ scenarios/HWshared/HwuDialogSystem/examples/experimentDialog.java
@@ -13,6 +13,7 @@ import java.util.List;
  * Am I missing any screen specific stuff?
  * How are we specifying memory case, or not? Somewhere in the migrationData?
  * Add some emotions/expressions in
+ * need logging
  */
 
 /*utility stuff up first */
@@ -54,9 +55,11 @@ failure() {
 	speak("Just head back to 1.54");
 	di.getResponse("I'm back");
 	
-	if (latest.equals("start")) 
-		speak("THIS ISN'T IMPLEMENTED YET.");
-	else
+	if (latest.equals("episode2")) {
+		migrationData.remove("phoneColourChoice");
+		migrationData.remove("firstWhy");
+		episode2startPhone();
+	}else
 		speak("Speak to Iain or Michael");	
 }
 
@@ -64,12 +67,20 @@ failure() {
 /* Migration related code*/
 
 migrateInDone() {
-	//TODO condition on what's been migrated in somehow
-	unBlankScreen
+	unBlankScreen();
 	
-	//TODO choose which case here
-	episode2PhoneMemory()
+	if (migrationData.get("episode").equals("1"))
+		episode2startPhone();
+	
 }
+migrateInDone() {
+	unBlankScreen();
+	
+	if (migrationData.get("episode").equals("2"))
+		episode3screen();
+	
+}
+
 requestMigration(String from) {
 	di.blankScreen();
 	Thread.sleep(500);
@@ -85,6 +96,11 @@ migrateOut(String to) {
 	while(!di.migrateDataOut(to,migrationData) && retries > 0) {
 		System.out.println("ERROR MIGRATING");
 		retries--;
+		Thread.sleep(1000);
+	}
+	if (retries == 0) {
+		speak("Oh dear, something's gone wrong.");
+		speak("Speak to Michael or Iain.");
 	}
 	//TODO need something to retry more maybe?
 	di.blankScreen();
@@ -94,15 +110,25 @@ migrateOut(String to) {
 
 //setup stuff
 
-setup(String participantID) {
+setup(String participantID, char track) {
 	migrationData.clear();
 	migrationData.put("participant",participantID);
+	String memory = "1,2,3,4,5,6,7,8,";
+	if (char == 'b') {
+		memory = "TODO";
+	} else if (char == 'c') {
+		memory = "TODO";
+	}
+	
+	
 	//remember to end on a comma!
-	migrationData.put("memory","1,2,3,4,5,6,7,8,");//a number present means it should remember, missing means forget previous
+	//migrationData.put("memory","1,2,3,4,5,6,7,8,");//a number present means it should remember, missing means forget previous
+	migrationData.put("memory",memory);
 	/*
 	 * What if we forget on 4 say, then need to remember from 2 in 8? shouldn't, it was
 	 * forgotten in 4. Don't want to actually remove it from memory...
 	 */
+	episode1Screen();
 }
 
 boolean remembers(int episode, int currentEpisode) {
@@ -117,6 +143,7 @@ boolean remembers(int episode, int currentEpisode) {
 //EPISODE 1
 
 episode1Screen() {
+	migrationData.put("episode","1");
 	speak("Hi there! So you've come for the treasure hunt, eh?");
 	di.getResponse("Yes");
 	speak("Great, let's first check we can communicate with each other");
@@ -147,6 +174,13 @@ episode1Screen() {
 /***********************************************/
 //Episode 2
 
+episode2startPhone() {
+	if (remembers(1,2))
+		episode2PhoneMemory();
+	else
+		episode2PhoneNoMemory();
+}
+
 episode2PhoneMemory() {
 	speak("Right, hermit, I think I know what that's about.");
 	episode2PhoneCommon();
@@ -163,6 +197,7 @@ episode2PhoneNoMemory() {
 	episode2PhoneCommon();
 }
 episode2PhoneCommon() {
+	migrationData.put("episode","2");
 	speak("before we go, it's a bit dark in here, I don't like that.");
 	speak("Can you set the backgrounf to my favourite colour?");
 	String[] optionsColours = {"Red","Blue","Yellow","Green","Purple","Pink","Orange","What's your favourite?"};
@@ -179,8 +214,11 @@ episode2PhoneCommon() {
 	}
 	speak("So now I'll take you to the place where you'll find the hermit");
 	di.getResponse("OK");
-	//TODO need a different callback for non-memory case!
-	di.startNav("1.54","1east1","episode2PhoneMemoryArrived()");
+	latest = "episode2";
+	if (remembers(1,2))
+		di.startNav("1.54","1east1","episode2PhoneMemoryArrived()");
+	else
+		di.startNav("1.54","1east1","episode2PhoneForgetArrived()");
 }
 episode2ArrivedCommon() {
 	Thread.sleep(3000);
@@ -194,10 +232,7 @@ episode2ArrivedCommon() {
 episode2PhoneMemoryArrived() {
 	episode2ArrivedCommon();
 	String clue1_answer = clue1_answer = di.getFreetext();;
-	while (!clue1_answer.contains("rog")) { //rog to ignore case of F
-		speak("Hmm, doesn't sound right to me");
-		clue1_answer = di.getFreetext();
-	}
+
 	migrationData.put("clue1_answer",clue1_answer);
 	ep2return();
 }
@@ -205,6 +240,7 @@ episode2PhoneForgetArrived() {
 	episode2ArrivedCommon();
 	String[] optionsAnswer= {"A lonely man","the singer of a band","a frog","a crab", "a pizza"};
 	String clue1_answer = di.multipleChoiceQuestion(5,optionsClues);
+
 	migrationData.put("clue1_answer",clue1_answer);
 	ep2return();
 }
@@ -225,15 +261,15 @@ waitForReturnToScreenEp2() {
 
 /***********************************************/
 //Episode 3
-//TODO there's an issue here. A difference between remembering phone embodiment
-//and remembering the previous screen embodiment too...
-//maybe we need to just condition on things being in the memory?
+
+
 episode3screen() {
 	//TODO whatever should happen to wait for a migration
 	//should this be called AFTER the migrate out above?
+	migrationData.put("episode","3");
 	if (remembers(2,3)) {
 		speak("Ok, we've got the first part of the answer.");
-		speak("According to you, hermit is a "+ migrateData.get("clue1_answer");
+		speak("According to you, hermit is "+ migrateData.get("clue1_answer");
 		speak("We'll find out at the end if that's right.");
 	}
 	String first_clue = migrationDate.get("first_clue");
@@ -259,6 +295,7 @@ episode3screen() {
 			second_clue = di.multipleChoiceQuestion(4,optionsClues);
 		}
 		migrationData.put("second_clue",second_clue);
+		//TODO log this stuff
 	}
 	//reveal the clue now
 	speak("OK, this clue is \"Angus\"."); //TODO or should it be submarine?
