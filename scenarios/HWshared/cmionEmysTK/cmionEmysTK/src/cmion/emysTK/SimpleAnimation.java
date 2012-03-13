5d909f019275fda011935afa7331eb1aabb0e253
michael.kriegel <michael.kriegel@dcc22606-d844-4505-a083-a3dbd7937ca2>
2012-03-13 14:51:27 +0000

    a few small changes to in the wild code in preparation for the treasure hunt experiment
    git-svn-id: http://svn.lirec.eu@1567 dcc22606-d844-4505-a083-a3dbd7937ca2

--- scenarios/HWshared/cmionEmysTK/cmionEmysTK/src/cmion/emysTK/SimpleAnimation.java
+++ scenarios/HWshared/cmionEmysTK/cmionEmysTK/src/cmion/emysTK/SimpleAnimation.java
@@ -11,6 +11,8 @@ public class SimpleAnimation extends Competency {
 	private String host;	
 	private int port;
 	
+	public static final String PARAMETER_RETURN_IMMEDIATELY = "returnImmediately";
+	
 	public SimpleAnimation(IArchitecture architecture, String host, String port) {
 		super(architecture);
 		this.host = host;
@@ -34,14 +36,20 @@ public class SimpleAnimation extends Competency {
 	protected boolean competencyCode(HashMap<String, String> parameters)
 			throws CompetencyCancelledException {
 		String animation = parameters.get("animation");
+		boolean returnImmediately = false;
+		if (parameters.containsKey(PARAMETER_RETURN_IMMEDIATELY))
+			returnImmediately = Boolean.parseBoolean(parameters.get(PARAMETER_RETURN_IMMEDIATELY));
 		if (animation == null) return false;
 		new SendCommandThread(host, port, "PlayAnimationForced" , animation).start();
 		// wait 3 seconds
-		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}		
+		if (!returnImmediately)
+		{
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 		return true;
 	}
 
